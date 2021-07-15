94646e7b45cb4da6d2b02ab14c8b77ce8c180fed
julianorodsousa <julianorodsousa@gmail.com>
2021-07-15 16:52:09 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Routes from "./src/Routes.js";
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./screen/Login/login";
+import { NavigationContainer } from '@react-navigation/native';
+
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Login/>
+    </NavigationContainer>
+  );
+}
+export default Routes;
