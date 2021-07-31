e544437bacd01e56ab2c4daeec8091fa4a04c02d
julianorodsousa <julianorodsousa@gmail.com>
2021-07-31 14:26:08 -0400

    PickerPet started

--- src/components/styled/picker.js
+++ src/components/styled/picker.js
@@ -1,23 +1,30 @@
 import { BlurView } from "@react-native-community/blur";
 import React, { useState, useEffect } from "react";
-import { Text, TouchableOpacity, View, Modal, StatusBar } from "react-native";
-import {
-  BlueBase,
-  GreenBase,
-  OrangeBase,
-  RedBase,
-  White,
-} from "../../assets/AppColors";
-import DropDownBlackIcon from "../../assets/images/DropDownBlackIcon.svg";
+import { Text, TouchableOpacity, View, Modal } from "react-native";
+import { BlueBase, GrayDark, GreenBase, RedBase } from "../../assets/AppColors";
 import { rem } from "../components";
+import { useAuth } from "../../hooks/Auth";
+import api from "../../services/api";
+import PetItem from "../Pets/PetItem";
 
-export const PickerState = (props) => {
-  const [text, setText] = useState("escolha um de seus pets");
-  const [textColor, setTextColor] = useState(RedBase);
+export function PickerState(props) {
+  const [text, setText] = useState("eu perdi meu pet");
+  const [GrayDark, setGrayDark] = useState(RedBase);
   const [petState, setPetState] = useState(1);
   const [visible, setVisible] = useState(false);
 
-  useEffect(() => {});
+  useEffect(() => {
+    if (petState == 1) {
+      setText("eu perdi meu pet");
+      setGrayDark(RedBase);
+    } else if (petState == 2) {
+      setText("eu encontrei um pet");
+      setGrayDark(BlueBase);
+    } else if (petState == 3) {
+      setText("esse pet está para adoção");
+      setGrayDark(GreenBase);
+    }
+  });
 
   return (
     <TouchableOpacity
@@ -26,7 +33,7 @@ export const PickerState = (props) => {
         props.style
           ? props.style
           : {
-              borderColor: textColor,
+              borderColor: GrayDark,
               borderWidth: 1,
               paddingHorizontal: 8 * rem,
               borderRadius: 20 * rem,
@@ -38,7 +45,7 @@ export const PickerState = (props) => {
     >
       <Text
         style={{
-          color: textColor,
+          color: GrayDark,
           fontFamily: "Delius",
           fontSize: 17 * rem,
           paddingVertical: 4 * rem,
@@ -70,6 +77,7 @@ export const PickerState = (props) => {
             <View style={{ opacity: 0.8 }}>
               <TouchableOpacity
                 onPress={() => {
+                  props.onChange(1);
                   setPetState(1);
                   setVisible(false);
                 }}
@@ -96,6 +104,7 @@ export const PickerState = (props) => {
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => {
+                  props.onChange(2);
                   setPetState(2);
                   setVisible(false);
                 }}
@@ -122,6 +131,7 @@ export const PickerState = (props) => {
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => {
+                  props.onChange(3);
                   setPetState(3);
                   setVisible(false);
                 }}
@@ -154,25 +164,53 @@ export const PickerState = (props) => {
       )}
     </TouchableOpacity>
   );
-};
+}
+
 export const PickerPet = (props) => {
-  const [text, setText] = useState("eu perdi meu pet");
-  const [textColor, setTextColor] = useState(RedBase);
-  const [petState, setPetState] = useState(1);
+  const [text, setText] = useState("escolha um de seus pets");
+  const [isMounted, setIsMounted] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [visible, setVisible] = useState(false);
+  const [petList, setPetList] = useState([]);
+  const { token } = useAuth();
 
   useEffect(() => {
-    if (petState == 1) {
-      setText("eu perdi meu pet");
-      setTextColor(RedBase);
-    } else if (petState == 2) {
-      setText("eu encontrei um pet");
-      setTextColor(BlueBase);
-    } else if (petState == 3) {
-      setText("esse pet está para adoção");
-      setTextColor(GreenBase);
+    setIsMounted(true);
+    loadPets();
+    return () => setIsMounted(false);
+  }, []);
+
+  async function loadPets() {
+    setLoading(true);
+    try {
+      const response = await api.get("/getpetbytoken", {
+        headers: {
+          token,
+        },
+      });
+      const petsInfo = response.data;
+      setPetList(petsInfo);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
     }
-  });
+  }
+
+  const PetContainer = petList.map((item) => (
+    // props.petName(item.firstName + " " + item.lastName),
+    // props.petPicture(item.picture_url),
+    // props.petType()
+    <PetItem
+      key={item.id}
+      petImage={{ uri: item.picture_url }}
+      petName={item.firstName + " " + item.lastName}
+      petDescription={"item.description"}
+      petAge={item.birthdate}
+      petType={"Cat"}
+      petSex={item.male ? "Macho" : "Fêmea"}
+    />
+  ));
 
   return (
     <TouchableOpacity
@@ -181,7 +219,7 @@ export const PickerPet = (props) => {
         props.style
           ? props.style
           : {
-              borderColor: textColor,
+              borderColor: GrayDark,
               borderWidth: 1,
               paddingHorizontal: 8 * rem,
               borderRadius: 20 * rem,
@@ -193,7 +231,7 @@ export const PickerPet = (props) => {
     >
       <Text
         style={{
-          color: textColor,
+          color: GrayDark,
           fontFamily: "Delius",
           fontSize: 17 * rem,
           paddingVertical: 4 * rem,
@@ -201,9 +239,6 @@ export const PickerPet = (props) => {
       >
         {text}
       </Text>
-      {/* <View style={{ paddingLeft: 4 * rem }}>
-        <DropDownBlackIcon height={7 * rem} width={13 * rem} />
-      </View> */}
       {visible ? (
         <Modal transparent={false} visible={visible}>
           <BlurView
@@ -222,86 +257,7 @@ export const PickerPet = (props) => {
             onPress={() => setVisible(false)}
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
-            <View style={{ opacity: 0.8 }}>
-              <TouchableOpacity
-                onPress={() => {
-                  setPetState(1);
-                  setVisible(false);
-                }}
-                style={{
-                  borderRadius: 20 * rem,
-                  borderColor: RedBase,
-                  borderWidth: 1,
-                  paddingHorizontal: 8 * rem,
-                  alignItems: "center",
-                  paddingVertical: 5,
-                  marginVertical: 5 * rem,
-                }}
-              >
-                <Text
-                  style={{
-                    color: RedBase,
-                    fontFamily: "Delius",
-                    fontSize: 17 * rem,
-                    paddingVertical: 5,
-                  }}
-                >
-                  eu perdi meu pet
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  setPetState(2);
-                  setVisible(false);
-                }}
-                style={{
-                  borderRadius: 20 * rem,
-                  borderColor: BlueBase,
-                  borderWidth: 1,
-                  paddingHorizontal: 8 * rem,
-                  alignItems: "center",
-                  paddingVertical: 5,
-                  marginVertical: 5 * rem,
-                }}
-              >
-                <Text
-                  style={{
-                    color: BlueBase,
-                    fontFamily: "Delius",
-                    fontSize: 17 * rem,
-                    paddingVertical: 5 * rem,
-                  }}
-                >
-                  eu encontrei esse pet
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  setPetState(3);
-                  setVisible(false);
-                }}
-                style={{
-                  borderRadius: 20 * rem,
-                  borderColor: GreenBase,
-                  borderWidth: 1,
-                  paddingHorizontal: 8 * rem,
-                  alignItems: "center",
-                  paddingVertical: 5 * rem,
-                  marginVertical: 5 * rem,
-                }}
-              >
-                <Text
-                  style={{
-                    color: GreenBase,
-                    fontFamily: "Delius",
-                    fontSize: 17 * rem,
-                    paddingVertical: 5 * rem,
-                  }}
-                >
-                  esse pet está para adoção
-                </Text>
-              </TouchableOpacity>
-            </View>
+            <View style={{ opacity: 0.8 }}>{PetContainer}</View>
           </TouchableOpacity>
         </Modal>
       ) : (
--- src/screen/CreatePost/CreatePost.js
+++ src/screen/CreatePost/CreatePost.js
@@ -1,109 +1,239 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { View, Text, Button, TouchableOpacity, TextInput, Touchable } from 'react-native';
-import { GrayDark, GrayLight, OrangeBase, RedBase, White } from '../../assets/AppColors';
-import { ButtonLight, ButtonOrange, rem } from '../../components/components';
-import PictureIcon from '../../assets/images/PictureIcon.svg';
-import LocationIcon from '../../assets/images/LocationIcon.svg';
-import DropDownBlackIcon from '../../assets/images/DropDownBlackIcon.svg';
-import FastImage from 'react-native-fast-image';
-import { useAuth } from '../../hooks/Auth';
-import { PickerState, PickerPet } from '../../components/styled/picker';
+import { useNavigation } from "@react-navigation/native";
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  TextInput,
+  Touchable,
+} from "react-native";
+import {
+  GrayDark,
+  GrayLight,
+  OrangeBase,
+  RedBase,
+  White,
+} from "../../assets/AppColors";
+import { ButtonLight, ButtonOrange, rem } from "../../components/components";
+import PictureIcon from "../../assets/images/PictureIcon.svg";
+import LocationIcon from "../../assets/images/LocationIcon.svg";
+import DropDownBlackIcon from "../../assets/images/DropDownBlackIcon.svg";
+import FastImage from "react-native-fast-image";
+import { useAuth } from "../../hooks/Auth";
+import { PickerState, PickerPet } from "../../components/styled/picker";
 
 function CreatePost() {
   const navigation = useNavigation();
+  const [petState, setPetState] = useState(1);
   const { user } = useAuth();
+  function handleChange(newValue) {
+    setPetState(newValue);
+  }
+
   return (
-    <View style={{ backgroundColor: OrangeBase, flex: 1, paddingVertical: 8 * rem }}>
-      <View style={{
-        width: '100%',
-        height: 488 * rem,
-        backgroundColor: White,
-        borderRadius: 20 * rem,
-        justifyContent: 'space-around'
-      }}>
-        <View style={{ height: 350 * rem, width: '100%', backgroundColor: White, borderRadius: 20 * rem }}>
-          <View style={{ alignItems: 'flex-start', width: '100%', marginHorizontal: 10 * rem, marginTop: 10 * rem }}>
+    <View
+      style={{ backgroundColor: OrangeBase, flex: 1, paddingVertical: 8 * rem }}
+    >
+      <View
+        style={{
+          width: "100%",
+          height: 488 * rem,
+          backgroundColor: White,
+          borderRadius: 20 * rem,
+          justifyContent: "space-around",
+        }}
+      >
+        <View
+          style={{
+            height: 350 * rem,
+            width: "100%",
+            backgroundColor: White,
+            borderRadius: 20 * rem,
+          }}
+        >
+          <View
+            style={{
+              alignItems: "flex-start",
+              width: "100%",
+              marginHorizontal: 10 * rem,
+              marginTop: 10 * rem,
+            }}
+          >
             <View
               style={{
                 width: 265 * rem,
-                alignSelf: 'flex-start',
+                alignSelf: "flex-start",
                 height: 105 * rem,
                 margin: 10 * rem,
-                alignItems: 'center'
-              }}>
-              <View style={{ flexDirection: 'row', width: '100%', alignItems: 'flex-end' }}>
-                <FastImage source={{ uri: user.profilePictureUrl }} style={{ height: 67 * rem, width: 67 * rem, borderRadius: 45 * rem }} />
-                <View style={{ marginHorizontal: 8 * rem, alignSelf: 'center' }}>
-                  <Text style={{ color: GrayDark, fontFamily: 'Delius', fontSize: 17 * rem }}>{user.firstname + ' ' + user.lastname}</Text>
-                  <PickerState/>
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  width: "100%",
+                  alignItems: "flex-end",
+                }}
+              >
+                <FastImage
+                  source={{ uri: user.profilePictureUrl }}
+                  style={{
+                    height: 67 * rem,
+                    width: 67 * rem,
+                    borderRadius: 45 * rem,
+                  }}
+                />
+                <View
+                  style={{ marginHorizontal: 8 * rem, alignSelf: "center" }}
+                >
+                  <Text
+                    style={{
+                      color: GrayDark,
+                      fontFamily: "Delius",
+                      fontSize: 17 * rem,
+                    }}
+                  >
+                    {user.firstname + " " + user.lastname}
+                  </Text>
+                  <PickerState onChange={setPetState} />
                 </View>
               </View>
-              <PickerPet/>
+
+              <PickerPet />
             </View>
-
           </View>
 
-
-
-
-
-
-
-          <View style={{ width: '100%', borderBottomWidth: 1, borderColor: GrayLight }}>
-            <Text style={{ fontFamily: 'Delius', fontSize: 10 * rem, alignSelf: 'center', marginBottom: 5 * rem }}>Ou adicione um abaixo</Text>
-            <Text style={{ alignSelf: 'flex-start', fontFamily: 'Delius', fontSize: 18 * rem, margin: 2 * rem }}>Nome do pet</Text>
-            <View style={{ alignSelf: 'center', borderTopWidth: 1, borderColor: GrayLight, width: 50 * rem, margin: 2 * rem }} />
-            <TextInput placeholder={'Informe o nome do pet'} style={{ alignSelf: 'flex-start', fontFamily: 'Delius', fontSize: 18 * rem, marginHorizontal: 2 * rem }} />
+          <View
+            style={{
+              width: "100%",
+              borderBottomWidth: 1,
+              borderColor: GrayLight,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "Delius",
+                fontSize: 10 * rem,
+                alignSelf: "center",
+                marginBottom: 5 * rem,
+              }}
+            >
+              Ou adicione um abaixo
+            </Text>
+            <Text
+              style={{
+                alignSelf: "flex-start",
+                fontFamily: "Delius",
+                fontSize: 18 * rem,
+                margin: 2 * rem,
+              }}
+            >
+              Nome do pet
+            </Text>
+            <View
+              style={{
+                alignSelf: "center",
+                borderTopWidth: 1,
+                borderColor: GrayLight,
+                width: 50 * rem,
+                margin: 2 * rem,
+              }}
+            />
+            <TextInput
+              placeholder={"Informe o nome do pet"}
+              style={{
+                alignSelf: "flex-start",
+                fontFamily: "Delius",
+                fontSize: 18 * rem,
+                marginHorizontal: 2 * rem,
+              }}
+            />
           </View>
-          <View style={{ width: '100%', marginVertical: 10 * rem }}>
-            <Text style={{ alignSelf: 'flex-start', fontFamily: 'Delius', fontSize: 18 * rem, margin: 2 * rem }}>
-              Descrição</Text>
-            <View style={{ alignSelf: 'center', borderTopWidth: 1, borderColor: GrayLight, width: 50 * rem, margin: 2 * rem }} />
-            <TextInput placeholder={'Adicione uma descrição'} style={{ alignSelf: 'flex-start', fontFamily: 'Delius', fontSize: 18 * rem, margin: 2 * rem }}></TextInput>
+          <View style={{ width: "100%", marginVertical: 10 * rem }}>
+            <Text
+              style={{
+                alignSelf: "flex-start",
+                fontFamily: "Delius",
+                fontSize: 18 * rem,
+                margin: 2 * rem,
+              }}
+            >
+              Descrição
+            </Text>
+            <View
+              style={{
+                alignSelf: "center",
+                borderTopWidth: 1,
+                borderColor: GrayLight,
+                width: 50 * rem,
+                margin: 2 * rem,
+              }}
+            />
+            <TextInput
+              placeholder={"Adicione uma descrição"}
+              style={{
+                alignSelf: "flex-start",
+                fontFamily: "Delius",
+                fontSize: 18 * rem,
+                margin: 2 * rem,
+              }}
+            ></TextInput>
           </View>
         </View>
 
-
-
-
-
-
-
-
-
-        <View style={{
-          borderRadius: 20 * rem,
-          width: '100%',
-          height: 135 * rem,
-          backgroundColor: White,
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          elevation: 3
-        }}>
-          <View style={{
-            width: 50 * rem, borderTopWidth: 1, elevation: 2, borderColor: GrayLight,
-            marginBottom: 5 * rem
-          }}>
-          </View>
-          <TouchableOpacity style={{ flexDirection: 'row', height: 34 * rem, width: '100%', alignItems: 'center', paddingHorizontal: 10 * rem }}>
+        <View
+          style={{
+            borderRadius: 20 * rem,
+            width: "100%",
+            height: 135 * rem,
+            backgroundColor: White,
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            elevation: 3,
+          }}
+        >
+          <View
+            style={{
+              width: 50 * rem,
+              borderTopWidth: 1,
+              elevation: 2,
+              borderColor: GrayLight,
+              marginBottom: 5 * rem,
+            }}
+          ></View>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              height: 34 * rem,
+              width: "100%",
+              alignItems: "center",
+              paddingHorizontal: 10 * rem,
+            }}
+          >
             <PictureIcon height={29 * rem} width={32 * rem} />
             <Text style={{ marginHorizontal: 5 * rem }}> Adicionar foto</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{
-            flexDirection: 'row', height: 34 * rem, width: '100%',
-            alignItems: 'center', paddingHorizontal: 10 * rem
-          }}>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              height: 34 * rem,
+              width: "100%",
+              alignItems: "center",
+              paddingHorizontal: 10 * rem,
+            }}
+          >
             <LocationIcon height={24 * rem} width={29 * rem} />
             <Text style={{ marginHorizontal: 5 * rem }}> Adicionar local</Text>
           </TouchableOpacity>
         </View>
       </View>
       <ButtonOrange
-        style={{ alignSelf: 'center', elevation: 3, marginVertical: 10 * rem }}
-        onPress={() => navigation.navigate('Preview')}
-        text={'Pronto'} />
+        style={{ alignSelf: "center", elevation: 3, marginVertical: 10 * rem }}
+        onPress={() => navigation.navigate("Preview")}
+        text={"Pronto"}
+      />
     </View>
-  )
+  );
 }
 export default CreatePost;
